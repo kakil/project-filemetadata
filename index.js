931808b29cd2ff259d4bfeef02d24b5005b9ab9e
kakil <kakil@me.com>
2023-09-22 17:58:04 +0000

    Completed metadata project.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
+const upload = multer({ dest: './public/data/uploads/' });
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
@@ -15,6 +17,13 @@ app.get('/', function (req, res) {
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
+});
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+
+
+  res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size });
 });
